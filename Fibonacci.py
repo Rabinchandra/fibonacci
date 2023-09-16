b5f855767a8bd7c4f4a72d7b6cbbe6b0dd864d64
Rabinchandra <47386147+Rabinchandra@users.noreply.github.com>
2023-09-16 21:49:29 +0530

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,19 @@
+# To print the fibonacci sequence upto nth term
+def fib_1(n, a=0, b=1):
+    if n > 0:
+        print(a, end=" ")
+        fib_1(n-1, b, a+b)
+
+
+# To get the nth term of the fibonacci sequence
+def fib_2(n, a=0, b=1):
+    if n <= 1:
+        return a
+    return fib_2(n-1, b, a+b)
+
+
+fib_1(5)  # print the fibonacci sequence upto 5th term
+
+print()  # next line
+
+print("Fibonacci Sequence of 5th term: ", fib_2())
